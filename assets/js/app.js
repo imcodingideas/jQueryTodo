38cd67be111e467b0c3e11c6d54c2c626de724d6
Joseph Chambers <joseph@michael-chambers.com>
2016-11-03 15:28:03 -0700

    added listeners to elements when the page loads and feature elements. Created a listener on the text input, and when you press enter it sets the value and appends it to the new li

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,27 @@
 console.log('connected');
 
 //check off specific todos by clicking
-
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
     $(this).toggleClass('completed')
 });
 
 // click on x to delete todo
-$('span').click(function (event) {
+$('ul').on('click', 'span', function (event) {
    $(this).parent().fadeOut(500, function () {
       $(this).remove()
    });
 
    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        //grab new todo text from input
+        var todoText = $(this).val();
+        //empty input
+        $(this).val("");
+
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
